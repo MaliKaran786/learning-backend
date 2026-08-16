5c21d12cd41550a35974ee70616f758daab2486b
MaliKaran786 <karanmalik203@gmail.com>
2026-08-16 18:30:49 +0530

    url and queries

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,25 @@
 const http=require("http")
 const fs=require("fs")
+const url=require("url")
+
 const myServer=http.createServer((req,res)=>{
+  if(req.url==="/favicon.ico") return res.end()
   const log=`${Date.now()}${req.url}New Request Received!\n`
+  const myUrl=url.parse(req.url,true)
+  console.log(myUrl)
   fs.appendFile("log.txt",log,(err,data)=>{
     console.log("New Request Received!!")
-    switch(req.url){
+    switch(myUrl.pathname){
       case `/`:res.end("HomePage")
       break;
-      case `/about`:res.end("Hello Karan Malik")
+      case `/about`:
+      const username=myUrl.query.name
+      res.end(`Hi ${username}`)
       break;
+      case '/search':
+        const search=myUrl.query.search_query
+        res.end('Here Are your search results for :'+search)
+        break;
       case `/Contact-us`:res.end("email")
       break;
       default:res.end("404")
