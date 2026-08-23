192969e600081c287c57592f5e6cd2292f53d5a2
MaliKaran786 <karanmalik203@gmail.com>
2026-08-23 15:49:32 +0530

    Postman & REST-API

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,9 +1,9 @@
 const express=require('express')
+const fs=require('fs')
 const users=require('./MOCK_DATA.json')
+const { json } = require('stream/consumers')
 const app=express()
-app.get('/api/users',(req,res)=>{
-  return res.json(users)
-})
+app.use(express.urlencoded({extended : false}))
 app.get('/users',(req,res)=>{
   const html=`
   <ul>
@@ -12,10 +12,34 @@ app.get('/users',(req,res)=>{
   `
   res.send(html)
 })
-app.get('/api/users/:id',(req,res)=>{
+app.get('/api/users',(req,res)=>{
+  return res.json(users)
+})
+app
+.route('/api/users/:id')
+.get((req,res)=>{
   const id=Number(req.params.id)
-  const user=users.find((user) =>user.id===id)
-  return  res.json(user)
+  const user=users.find((user)=> user.id===id)
+  return res.json(user)
+})
+.patch((req,res)=>{
+  const id=Number(req.params.id)
+  const user=users.find((user)=>user.id===id)
+  user.first_name="Hello"
+  return res.json(users)
+})
+.delete((req,res)=>{
+  const id=Number(req.params.id)
+  const newArr=users.filter((user)=>user.id != id)
+  console.log("deleted SUer with id :",id)
+  return res.json(newArr)
+})
+app.post('/api/users',(req,res)=>{
+  const body=req.body
+  users.push({...body,id:users.length + 1})
+  fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+    return res.json({Status : "Success",id: users.length })
+  })
 })
 const PORT=3002
 
