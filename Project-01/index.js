e13f1cb1d5fb6bf4bf398c9a10589df32a941ca3
MaliKaran786 <karanmalik203@gmail.com>
2026-08-26 20:26:12 +0530

    Status Codes

--- Project-01/index.js
+++ Project-01/index.js
@@ -31,6 +31,7 @@ app
 .get((req,res)=>{
   const id=Number(req.params.id)
   const user=users.find((user)=> user.id===id)
+  if(!user) return res.status(404).json({msg:"User Not found!!"})
   return res.json(user)
 })
 .patch((req,res)=>{
@@ -47,9 +48,12 @@ app
 })
 app.post('/api/users',(req,res)=>{
   const body=req.body
+  if(!body || !body.first_name || !body.last_name || !body.email || !body.gender ||!body.job_title){
+    return res.status(400).json({msg: "All fields are required..."})
+  }
   users.push({...body,id:users.length + 1})
   fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-    return res.json({Status : "Success",id: users.length })
+    return res.status(201).json({Status : "Success",id: users.length })
   })
 })
 const PORT=3002
